80e3aba0b941eb811b00e88e5d515a882dc40c08
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2020-08-13 22:53:37 +0400

    state checked item

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default class App extends Component {
       todoItems: [
         { action: "Купить цветы", done: false },
         { action: "Купить обувь", done: false },
-        { action: "Collect Коллекция билетов", done: true },
-        { action: "Пощвонить Виктору", done: false }
+        { action: "Коллекция билетов", done: true },
+        { action: "Позвонить Виктору", done: false }
       ],
       newItemText: ""
     }
@@ -21,14 +21,23 @@ export default class App extends Component {
     if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
       this.setState({ todoItems: [...this.state.todoItems, { action: this.state.newItemText, done: false }], newItemText: '' })
     }
-    console.log(this.state.todoItems);
   }
+  toggleTodo = (todo) => this.setState({
+    todoItems: this.state.todoItems.map(item => item.action === todo.action ? { ...item, done: !item.done } : item)
+  });
+  todoTableRows = () => this.state.todoItems.map(item =>
+    <tr key={item.action}>
+      <td>{item.action}</td>
+      <td> <input type='checkbox' checked={item.done} onChange={() => this.toggleTodo(item)} /></td>
+    </tr>
+  )
+
   render() {
     return (
       <div>
         <h4 className="bg-info text-white text-center p-2">
           Привет {this.state.userName}!
-          ({this.state.todoItems.filter(todo => !todo.done).length} элемент списка)
+          (Осталось не выбрано {this.state.todoItems.filter(todo => !todo.done).length} задания)
       </h4>
         <div className="container-fluid">
           <div className="my-1">
@@ -36,8 +45,14 @@ export default class App extends Component {
               value={this.state.newItemText}
               onChange={this.updateNewTextValue} />
             <button className="btn btn-primary mt-1"
-              onClick={this.createNewTodo}>Add</button>
+              onClick={this.createNewTodo}>Добавить задание</button>
           </div>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr><th>Description</th><th>Done</th></tr>
+            </thead>
+            <tbody>{this.todoTableRows()}</tbody>
+          </table>
         </div>
       </div>
     );
