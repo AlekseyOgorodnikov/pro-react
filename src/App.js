17e1e512b6a060fd1f09f3849e4a1c457c1b9576
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2020-08-14 13:48:11 +0400

    state checked words

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {TodoBanner} from './TodoBanner';
+import { TodoCreator } from "./TodoCreator";
+import { TodoRow } from "./TodoRow";
 
 export default class App extends Component {
   constructor(props) {
@@ -11,34 +14,49 @@ export default class App extends Component {
         { action: "Коллекция билетов", done: true },
         { action: "Позвонить Виктору", done: false }
       ],
-      newItemText: ""
+      // newItemText: ""
     }
   }
+
   updateNewTextValue = (event) => {
     this.setState({ newItemText: event.target.value })
   }
+
   createNewTodo = () => {
     if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
       this.setState({ todoItems: [...this.state.todoItems, { action: this.state.newItemText, done: false }], newItemText: '' })
     }
   }
+
   toggleTodo = (todo) => this.setState({
     todoItems: this.state.todoItems.map(item => item.action === todo.action ? { ...item, done: !item.done } : item)
   });
+
   todoTableRows = () => this.state.todoItems.map(item =>
     <tr key={item.action}>
       <td>{item.action}</td>
       <td> <input type='checkbox' checked={item.done} onChange={() => this.toggleTodo(item)} /></td>
     </tr>
   )
-
+  chagedItemTodo = () => {
+    const itemTodo = this.state.todoItems.filter(todo => !todo.done).length
+    const textSet = 'У вас'
+    if (itemTodo === 0) {
+      return 'У вас нет не оконченых дел'
+    } else if (itemTodo === 1) {
+      return textSet + ' ' + itemTodo + ' неоконченное дело'
+    } else if (itemTodo >= 2 && itemTodo <= 4) {
+      return textSet + ' ' + itemTodo + ' неоконченных дела'
+    } else if (itemTodo >= 5) {
+      return textSet + ' ' + itemTodo + ' неоконченных дел'
+    }
+  }
   render() {
     return (
       <div>
         <h4 className="bg-info text-white text-center p-2">
-          Привет {this.state.userName}!
-          (Осталось не выбрано {this.state.todoItems.filter(todo => !todo.done).length} задания)
-      </h4>
+          Привет {this.state.userName}! <span>({this.chagedItemTodo()})</span>
+        </h4>
         <div className="container-fluid">
           <div className="my-1">
             <input className="form-control"
@@ -49,7 +67,7 @@ export default class App extends Component {
           </div>
           <table className="table table-striped table-bordered">
             <thead>
-              <tr><th>Description</th><th>Done</th></tr>
+              <tr><th>Описание</th><th>Выбрано/Не выбрано</th></tr>
             </thead>
             <tbody>{this.todoTableRows()}</tbody>
           </table>
